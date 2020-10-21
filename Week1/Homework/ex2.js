f119876b59e9ecc406ddc48d4153541e59794695
Sadeq Al-Dallal <66870615+sadeq93@users.noreply.github.com>
2020-10-21 12:30:23 +0200

    changed function name to "run_mysql_query"

--- Week1/Homework/ex2.js
+++ Week1/Homework/ex2.js
@@ -1,29 +1,29 @@
 const mysql = require("mysql");
-const connections ={
-    host: "localhost",
-    user: "hyfuser",
-    password: "hyfpassword",
-    database: "new_world"
-  }
+const connections = {
+  host: "localhost",
+  user: "hyfuser",
+  password: "hyfpassword",
+  database: "new_world"
+}
 const db = mysql.createConnection(connections);
-  db.connect((err) => {
-    if(err) throw err;
-    console.log('connecting ...')
-  });
-function createMySQL_Query (mySQL_Command){
-  db.query(mySQL_Command,(error,results,fields)=>{
-    if (error) throw error ;
-    console.table(results)
+db.connect((err) => {
+  if (err) throw err;
+  console.log('connecting ...')
 });
+function run_mysql_query(mySQL_Command) {
+  db.query(mySQL_Command, (error, results, fields) => {
+    if (error) throw error;
+    console.table(results)
+  });
 }
 
-createMySQL_Query('SELECT Name FROM country WHERE Population > 8000000');
-createMySQL_Query('SELECT Name FROM country WHERE name LIKE "%land%"');
-createMySQL_Query('SELECT Name FROM country WHERE Population BETWEEN 500000 AND 1000000');
-createMySQL_Query('SELECT Name FROM country ORDER BY SurfaceArea DESC' );
-createMySQL_Query('SELECT Name FROM City where CountryCode = "NLD"' );
-createMySQL_Query('SELECT Population FROM City where Name = "Rotterdam"' );
-createMySQL_Query('SELECT Name FROM country ORDER BY SurfaceArea DESC LIMIT 10' );
-createMySQL_Query('SELECT Name FROM country ORDER BY Population DESC LIMIT 10' );
-createMySQL_Query('SELECT Population FROM country ORDER BY Population DESC LIMIT 1' );
+run_mysql_query('SELECT Name FROM country WHERE Population > 8000000');
+run_mysql_query('SELECT Name FROM country WHERE name LIKE "%land%"');
+run_mysql_query('SELECT Name FROM country WHERE Population BETWEEN 500000 AND 1000000');
+run_mysql_query('SELECT Name FROM country ORDER BY SurfaceArea DESC');
+run_mysql_query('SELECT Name FROM City where CountryCode = "NLD"');
+run_mysql_query('SELECT Population FROM City where Name = "Rotterdam"');
+run_mysql_query('SELECT Name FROM country ORDER BY SurfaceArea DESC LIMIT 10');
+run_mysql_query('SELECT Name FROM country ORDER BY Population DESC LIMIT 10');
+run_mysql_query('SELECT Population FROM country ORDER BY Population DESC LIMIT 1');
 db.end();
